e383594f2eca87d92509e77d91b14c0b3746e4c1
ridhaaf <ridhaaf@gmail.com>
2022-09-24 22:35:09 +0700

    Initial Commit

---
+++ LatihanMengunduhGambar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LatihanMengunduhGambar
+//
+//  Created by Ridha Ahmad Firdaus on 24/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
